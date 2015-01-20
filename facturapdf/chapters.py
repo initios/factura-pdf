c7215dccb4fd677b1a3d2920e4347a150ba47bd5
Carlos Goce <carlosgoce@gmail.com>
2015-01-20 12:51:54 +0100

    Add chapters module with Element function, which parse a string and creates an element based on it

---
+++ facturapdf/chapters.py
@@ -0,0 +1,22 @@
+import re
+from reportlab import platypus
+from facturapdf import flowables
+
+
+def element(item):
+    elements = {
+        'framebreak': platypus.FrameBreak,
+        'simpleline': flowables.SimpleLine,
+    }
+
+    match = re.search('(?P<name>\w+)(\[(?P<args>.+)\])?', item)
+
+    if match and match.group('name') in elements:
+        args = [] if not match.group('args') else match.group('args').split(',')
+        return elements[match.group('name')](*args)
+
+    return item
+
+
+def chapter(data):
+    pass
--- tests/tests_unit.py
+++ tests/tests_unit.py
@@ -1,9 +1,15 @@
 # coding=utf-8
+
 import unittest
+
+from reportlab import platypus
+
+from facturapdf import flowables
+from facturapdf.chapters import chapter, element
 from facturapdf.helper import chunks
 
 
-class TestHelper(unittest.TestCase):
+class HelperTest(unittest.TestCase):
     def test_chunks(self):
         collection = [1, 2, 3, 4, 5]
         chunks_collection = chunks(collection, 2)
@@ -15,3 +21,12 @@ class TestHelper(unittest.TestCase):
         chunks_collection = chunks(collection, 2, 9)
 
         self.assertEqual([[1, 2], [3, 4], [5, 9]], chunks_collection)
+
+
+class ElementTest(unittest.TestCase):
+    def test_can_create_frame_breaks(self):
+        self.assertIsInstance(element('framebreak'), platypus.doctemplate._FrameBreak)
+
+    def test_can_create_simple_line_flowables(self):
+        self.assertIsInstance(element('simpleline[185,0.1]',), flowables.SimpleLine)
+        
